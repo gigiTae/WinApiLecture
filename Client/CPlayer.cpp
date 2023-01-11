6bafbebb3a33241706870fca67450cacf99ea251
gigiTae <rlxo800@gmail.com>
2023-01-11 17:44:15 +0900

    RigidBody(3): 중력 시작

--- Client/CPlayer.cpp
+++ Client/CPlayer.cpp
@@ -17,8 +17,12 @@
 
 CPlayer::CPlayer()
 	: m_pTex(nullptr)
+	, m_eCurState(PLAYER_STATE::IDLE)
+	, m_iDir(KEY::S)
+	, m_ePrevState(PLAYER_STATE::IDLE)
+
 {
-	m_pTex = CResMgr::GetInst()->LoadTexture(L"Player1Tex", L"texture\\c1.bmp");
+	//m_pTex = CResMgr::GetInst()->LoadTexture(L"Player1Tex", L"texture\\c1.bmp");
 
 	CreateCollider();
 	GetCollider()->SetOffsetPos(Vec2(0.f, 10.f));
@@ -26,16 +30,17 @@ CPlayer::CPlayer()
 	
 	CreateRigidBody();
 
-	//// Texture 로딩하기
-	//CTexture* pTex = CResMgr::GetInst()->LoadTexture(L"PlayerTex", L"texture\\PlayerAnimation.bmp");
-	//
-	//CreateAnimator();
-	//GetAnimator()->CreateAnimation(L"WALK_DOWN", pTex, Vec2(0.f, 0.f), Vec2(64.f, 64.f), Vec2(64.f, 0.f), 0.1f, 3);
-	//GetAnimator()->Play(L"WALK_DOWN", true);
+	// Texture 로딩하기
+	CTexture* pTex = CResMgr::GetInst()->LoadTexture(L"PlayerTex", L"texture\\PlayerAnimation.bmp");
+	
+	CreateAnimator();
 
-	//CAnimation* pAnim = GetAnimator()->FindAnimation(L"WALK_DOWN");
-	//for (UINT i = 0; i < pAnim->GetMaxFrame(); ++i)
-	//	pAnim->GetFrame(i).vOffset = Vec2(0.f, -20.f);
+	GetAnimator()->CreateAnimation(L"IDLE", pTex, Vec2(0.f, 0.f), Vec2(64.f, 64.f), Vec2(64.f, 0.f), 0.2f, 3);
+	GetAnimator()->CreateAnimation(L"WALK_LEFT", pTex, Vec2(0.f, 192.f), Vec2(64.f, 64.f), Vec2(64.f, 0.f), 0.2f, 4);
+	GetAnimator()->CreateAnimation(L"WALK_RIGHT", pTex, Vec2(0.f, 64.f), Vec2(64.f, 64.f), Vec2(64.f, 0.f), 0.2f, 4);
+	GetAnimator()->CreateAnimation(L"WALK_UP", pTex, Vec2(0.f, 128.f), Vec2(64.f, 64.f), Vec2(64.f, 0.f), 0.2f, 4);
+	GetAnimator()->CreateAnimation(L"WALK_DOWN", pTex, Vec2(0.f, 0.f), Vec2(64.f, 64.f), Vec2(64.f, 0.f), 0.2f, 4);
+	GetAnimator()->Play(L"IDLE", true);
 
 }
 
@@ -46,44 +51,13 @@ CPlayer::~CPlayer()
 
 void CPlayer::update()
 {
-	CRigidBody* pRigid = GetRigidBody();
+	update_gravity();
 
-	if (KEY_HOLD(KEY::W))
-	{
-		pRigid->AddForce(Vec2 (0.f, -200.f));
-	}
-	if (KEY_TAP(KEY::W))
-	{
-		pRigid->AddVelocity(Vec2(0.f, -100.f));
-	}
+	update_move();
 
-	if (KEY_HOLD(KEY::S))
-	{
-		pRigid->AddForce(Vec2(0.f, 200.f));
-	}
-	if (KEY_TAP(KEY::S))
-	{
-		pRigid->AddVelocity(Vec2(0.f, 100.f));
-	}
+	update_state();
 
-	if (KEY_HOLD(KEY::A))
-	{
-		pRigid->AddForce(Vec2(-100.f, 0.f));
-	}
-	if (KEY_TAP(KEY::A))
-	{
-		pRigid->AddVelocity(Vec2(-100.f, 0.f));
-	}
-
-	if (KEY_HOLD(KEY::D))
-	{
-		pRigid->AddForce(Vec2(200.f, 0.f));
-	}
-	if (KEY_TAP(KEY::D))
-	{
-		pRigid->AddVelocity(Vec2(100.f, 0.f));
-	}
-
+	update_animation();
 
 
 	if (KEY_TAP(KEY::SPACE))
@@ -91,31 +65,33 @@ void CPlayer::update()
 		CreateMissile();
 	}
 
-
 	GetAnimator()->update();
+
+	m_ePrevState = m_eCurState;
 }
 
 void CPlayer::render(HDC _dc)
 {
-	int iWidth = (int)m_pTex->Whidth();
-	int iHeight = (int)m_pTex->Height();
 
-	Vec2 vPos = GetPos();
-	vPos = CCamera::GetInst()->GetRenderPos(vPos);
-	
-	BLENDFUNCTION bf = {};
-	bf.BlendOp = AC_SRC_OVER;
-	bf.BlendFlags = 0;
-	bf.AlphaFormat = 0;
-	bf.SourceConstantAlpha = 126;
+	//int iWidth = (int)m_pTex->Whidth();
+	//int iHeight = (int)m_pTex->Height();
 
-	AlphaBlend(_dc
-		, (int)(vPos.x - (float)(iWidth / 2))
-		, (int)(vPos.y - (float)(iHeight / 2))
-		, iWidth, iHeight
-		, m_pTex->GetDC()
-		, 0, 0, iWidth, iHeight
-		, bf);
+	//Vec2 vPos = GetPos();
+	//vPos = CCamera::GetInst()->GetRenderPos(vPos);
+	//
+	//BLENDFUNCTION bf = {};
+	//bf.BlendOp = AC_SRC_OVER;
+	//bf.BlendFlags = 0;
+	//bf.AlphaFormat = 0;
+	//bf.SourceConstantAlpha = 126;
+
+	//AlphaBlend(_dc
+	//	, (int)(vPos.x - (float)(iWidth / 2))
+	//	, (int)(vPos.y - (float)(iHeight / 2))
+	//	, iWidth, iHeight
+	//	, m_pTex->GetDC()
+	//	, 0, 0, iWidth, iHeight
+	//	, bf);
 
 
 	// 컴포넌트 (충돌체 ,etc...)가 있는경우
@@ -136,5 +112,116 @@ void CPlayer::CreateMissile()
 	pMissile->SeDir(0.f, -1.f);
 
 	CreateObject(pMissile, GROUP_TYPE::PROJ_PLAYER );
+}
+
+void CPlayer::update_state()
+{
+	if (KEY_TAP(KEY::A))
+	{
+		m_iDir = KEY::A;
+		m_eCurState = PLAYER_STATE::WALK;
+	}
+	if (KEY_TAP(KEY::D))
+	{
+		m_iDir = KEY::D;
+		m_eCurState = PLAYER_STATE::WALK;
+	}
+	if (KEY_TAP(KEY::W))
+	{
+		m_iDir = KEY::W;
+		m_eCurState = PLAYER_STATE::WALK;
+	}
+	if (KEY_TAP(KEY::S))
+	{
+		m_iDir = KEY::S;
+		m_eCurState = PLAYER_STATE::IDLE;
+	}
+
+}
+
+void CPlayer::update_move()
+{
+
+	CRigidBody* pRigid = GetRigidBody();
+
+	if (KEY_HOLD(KEY::W))
+	{
+		pRigid->AddForce(Vec2(0.f, -200.f));
+	}
+	if (KEY_TAP(KEY::W))
+	{
+		pRigid->AddVelocity(Vec2(0.f, -100.f));
+	}
+
+	if (KEY_HOLD(KEY::S))
+	{
+		pRigid->AddForce(Vec2(0.f, 200.f));
+	}
+	if (KEY_TAP(KEY::S))
+	{
+		pRigid->AddVelocity(Vec2(0.f, 100.f));
+	}
+
+	if (KEY_HOLD(KEY::A))
+	{
+		pRigid->AddForce(Vec2(-200.f, 0.f));
+	}
+	if (KEY_TAP(KEY::A))
+	{
+		pRigid->AddVelocity(Vec2(-100.f, 0.f));
+	}
+
+	if (KEY_HOLD(KEY::D))
+	{
+		pRigid->AddForce(Vec2(200.f, 0.f));
+	}
+	if (KEY_TAP(KEY::D))
+	{
+		pRigid->AddVelocity(Vec2(100.f, 0.f));
+	}
+
+}
+
+void CPlayer::update_animation()
+{
+	if (m_ePrevState != m_eCurState)
+		return;
+
+	switch (m_eCurState)
+	{
+	case PLAYER_STATE::IDLE:
+	{
+		GetAnimator()->Play(L"IDLE", true);
+	}
+		break;
+	case PLAYER_STATE::WALK:
+	{
+		if (m_iDir == KEY::D)
+			GetAnimator()->Play(L"WALK_RIGHT", true);
+		else if (m_iDir == KEY::A)
+			GetAnimator()->Play(L"WALK_LEFT", true);
+		else if (m_iDir == KEY::W)
+			GetAnimator()->Play(L"WALK_UP", true);
+		else if (m_iDir == KEY::S)
+			GetAnimator()->Play(L"WALK_DOWN", true);
+	}
+
+		break;
+	case PLAYER_STATE::ATTACK:
+
+		break;
+	case PLAYER_STATE::DEAD:
+
+		break;
+	default:
+
+		break;
+	}
+}
+
+void CPlayer::update_gravity()
+{
+//	GetRigidBody()->AddForce(Vec2(0.f,500.f));
+
 }
 
--- Client/CPlayer.h
+++ Client/CPlayer.h
@@ -1,12 +1,37 @@
 #pragma once
 #include "CObject.h"
+
+enum class PLAYER_STATE
+{
+    IDLE,
+    WALK,
+    ATTACK,
+    DEAD,
+};
+
+enum class PLAYER_ATTACK_STATE
+{
+    NOMAL_ATT_1,
+    NOMAL_ATT_2,
+    NOMAL_ATT_3,
+
+    SKILL_ATT_1,
+    // ..
+};
+
+
+enum class KEY;
 class CTexture;
 
 class CPlayer :
     public CObject
 {
 private:
-    CTexture* m_pTex;
+    CTexture*   m_pTex;
+
+    PLAYER_STATE   m_eCurState;
+    PLAYER_STATE   m_ePrevState;
+    KEY            m_iDir;
 
 public:
     virtual void update();
@@ -14,6 +39,10 @@ public:
 
 private:
     void CreateMissile();
+    void update_state();
+    void update_move();
+    void update_animation();
+    void update_gravity();
 
     CLONE(CPlayer);
 public:
